d1cb93c8cc916d8e02f31a2242b57d31046b2197
hridayakandel <hridayakandel@gmail.com>
2022-11-09 23:35:06 -0600

    clock fixed
    Month requires a +1 to be accurate, it starts counting at 0
    .getMonth() + 1  // month
    .getDate()

--- docs/Lab5/js/clock.js
+++ docs/Lab5/js/clock.js
@@ -6,7 +6,7 @@ window.onload = function () {
 
     setInterval(() => {
         const dateAndTime = new Date();
-        const result = `${dateAndTime.getFullYear()}-${dateAndTime.getMonth()}-${dateAndTime.getDay()}  ${dateAndTime.getHours()} : ${dateAndTime.getMinutes()} : ${dateAndTime.getSeconds()}`;
+        const result = `${dateAndTime.getFullYear()}-${dateAndTime.getMonth() + 1}-${dateAndTime.getDate()}  ${dateAndTime.getHours()} : ${dateAndTime.getMinutes()} : ${dateAndTime.getSeconds()}`;
         document.getElementById('clock').innerText = result;
     }, 1000);
 
